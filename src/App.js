ad3b864ece978d167abec139af54e01187255367
szczepaniakmichal <mssk87@gmail.com>
2022-01-30 21:47:38 +0100

    added logic

--- src/App.js
+++ src/App.js
@@ -4,15 +4,137 @@ import 'App.css';
 import Button from "components/Button/Button";
 import InputField from "components/InputField/InputField";
 import InputMathOperators from "components/InputMathOperators/InputMathOperators";
+import { useEffect } from "react";
 
+var stringMath = require('string-math');
 
 function App() {
-  return (
+
+useEffect(() => {
+    const btnCheckAllTask = document.querySelector('.btn-check-task');
+
+    function removeChild(nodeParent) {
+        while (nodeParent.firstChild) {
+            nodeParent.removeChild(nodeParent.firstChild)
+        }
+    }
+
+    function getMathOperation() {
+        const mathOperationArray = [];
+        [...document.querySelectorAll('.math-operation input')].filter(mathOperation => (mathOperation.checked ? mathOperationArray.push(mathOperation.value) : null));
+        return mathOperationArray;
+    }
+
+    function addTask(howManyTask, nodeParent, child, howManyValue, toValue) {
+        // console.log(arguments);
+
+        const mathOperators = getMathOperation();
+
+        for (let i = 0; i < howManyTask; i++) {
+            const task = document.createElement(child);
+            task.classList.add('task-list__item');
+            const numberValue = [];
+
+            for (let j = 0; j < howManyValue; j++) {
+                const number = Math.floor(Math.random() * toValue + 1);
+                numberValue.push(number);
+            }
+
+            // generate every numbers in <span>
+            const matchTask = numberValue.map((number, index) => {
+                const randomMathOperator = mathOperators[Math.floor(Math.random() * mathOperators.length)] || '+';
+                return (
+                    `<span class="number">${number}</span>
+                ${index === numberValue.length - 1 ? '<span> = </span>' : `<span>${randomMathOperator} </span>`}
+`
+                )
+            })
+
+            task.innerHTML = `
+        <span class="oneTask--js">${matchTask.join('')}</span>
+        <span>
+            <label for="result${i}"></label>
+            <input type="number" class="task-list__result" id="result${i}">
+        </span>
+        `;
+            nodeParent.appendChild(task);
+        }
+    }
+
+    function generateTask() {
+        const howManyTask = Number(document.querySelector(".how-many-task").value);
+        console.log("howManyTask", howManyTask)
+        const howManyValue = Number(document.querySelector(".how-many-value").value);
+        console.log("howManyTask", howManyTask)
+        const toValue = Number(document.querySelector(".to-value").value);
+        const taskList = document.querySelector('.task-list');
+        removeChild(taskList);
+        getMathOperation();
+        addTask(howManyTask, taskList, 'li', howManyValue, toValue);
+    }
+
+    function checkAllTask() {
+        const allTask = [...document.querySelectorAll(".task-list__item")];
+
+        allTask.forEach(task => {
+            const oneTask = task.querySelector('.oneTask--js');
+            const allNumbers = [...oneTask.querySelectorAll('span')].map(span => parseInt(span.textContent) ? parseInt(span.textContent) : span.textContent);
+            const answerInput = task.querySelector('.task-list__result');
+            const userAnswer = parseFloat(answerInput.value);
+            const resultText = task.querySelector('.result-text');
+
+            if (resultText) {
+                task.removeChild(resultText);
+            }
+
+            if (answerInput.value.length <= 0) {
+                answerInput.classList.remove('in-correct');
+                answerInput.classList.remove('correct')
+                return answerInput.setAttribute("placeholder", "complete the result")
+            };
+
+            const correctAnswer = document.createElement('span');
+            correctAnswer.classList.add('result-text');
+            correctAnswer.classList.add('correct-answer');
+            correctAnswer.textContent = 'Super to poprawny wynik :)';
+
+            const inCorrectAnswer = document.createElement('span');
+            inCorrectAnswer.classList.add('result-text');
+            inCorrectAnswer.classList.add('in-correct-answer');
+            inCorrectAnswer.textContent = 'Upsss... spróbuj jeszcze raz';
+
+            let counter = Number(stringMath(allNumbers.join('')).toFixed(2));
+
+            if (counter === userAnswer) {
+                answerInput.classList.add('correct');
+                answerInput.classList.remove('in-correct');
+                task.appendChild(correctAnswer);
+            } else {
+                answerInput.classList.add('in-correct');
+                answerInput.classList.remove('correct')
+                task.appendChild(inCorrectAnswer);
+            }
+        })
+    }
+
+    function test() {
+        const generateBtn = document.querySelector('.btn-generate-task-js');
+        generateBtn.addEventListener('click', generateTask);
+        // generateBtn.click();
+    }
+
+
+    btnCheckAllTask.addEventListener('click', checkAllTask);
+
+    test();
+})
+
+    return (
     <Div padding={20} column>
         <Div column>
-            <InputField title="how many tasks?" />
-            <InputField title="how many values to calculate?" />
-            <InputField title="maximum single value?"/>
+            <InputField title="how many tasks?" className="how-many-task"/>
+            <InputField title="how many values to calculate?" className="how-many-value"/>
+            <InputField title="maximum single value?" className="to-value"/>
         </Div>
         <Div column>
             <p>Type of mathematical operations:</p>
@@ -24,9 +146,10 @@ function App() {
         <Div column>
             <Button label='generate task'
                     isBolder
+                    className="btn-generate-task-js"
             />
-            <Button label='check task'
-            />
+            <ul className="task-list"></ul>
+            <Button label='check task' className='btn-check-task'/>
         </Div>
     </Div>
   );
--- src/components/InputField/InputField.js
+++ src/components/InputField/InputField.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 import { Label, Input, WrapperInputField } from './styles';
 
-function InputField({title}) {
+function InputField({title, value, className}) {
     return (
         <WrapperInputField>
             <Label>{title}
-                <Input type="number" className="input-value how-many-task"
+                <Input type="number" {...{value, className}}
                        placeholder={title} />
             </Label>
         </WrapperInputField>
