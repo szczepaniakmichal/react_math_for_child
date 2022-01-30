b89b0cc7bf56e7c6d9c11e2d14300a52643fbe73
szczepaniakmichal <mssk87@gmail.com>
2022-01-30 16:09:25 +0100

    added static elements to App structure

--- src/App.js
+++ src/App.js
@@ -1,21 +1,27 @@
 import Div from 'styled-kit/Div';
 
 import './App.css';
-import Button from "./components/Button";
+import Button from "./components/Button/Button";
+import InputField from "./components/InputField/InputField";
 
 
 function App() {
   return (
-    <>
-      <h1>math for child</h1>
-        <Div listLeft={10}>
+    <Div padding={20} column>
+        <Div column>
+        <InputField title="how many tasks?" />
+        <InputField title="how many values to calculate?" />
+        <InputField title="maximum single value?"/>
+        </Div>
+
+        <Div column>
             <Button label='generate task'
                     isBolder
             />
             <Button label='check task'
             />
         </Div>
-    </>
+    </Div>
   );
 }
 
