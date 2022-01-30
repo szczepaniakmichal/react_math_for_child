184bdeedd625ebd795c188ff6749bc6205a5369f
szczepaniakmichal <mssk87@gmail.com>
2022-01-30 16:08:20 +0100

    move Button and change page title

--- src/components/Button/styles.js
+++ src/components/Button/styles.js
@@ -1,11 +1,12 @@
 import styled from "styled-components";
 
-import { black, orange, white } from "../colors";
-import { paddingButton } from "../variables";
+import { black, orange, white } from "../../colors";
+import { paddingButton } from "../../variables";
 
 export const Wrapper = styled.button`
   width: max-content;
   padding: ${paddingButton};
+  margin: 10px 0;
   background-color: ${(props) => props.backgroundColor ? props.backgroundColor : orange };
   color: ${(props) => props.color ? props.color : black};
   font-size: ${(props) => props.fontSize ? props.fontSize : '1.5rem' };
