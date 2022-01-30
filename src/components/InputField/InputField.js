a0c49c1215eb25ae485f64cdbd1e3bc4b4f99f93
szczepaniakmichal <mssk87@gmail.com>
2022-01-30 16:09:51 +0100

    added component InputField.js

---
+++ src/components/InputField/InputField.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Div from 'styled-kit/Div';
+
+import { Label, Input } from './styles';
+
+function InputField({title, ...props}) {
+    return (
+        <Div padding="5px 0">
+            <Label>{title}
+                <Input type="number" className="input-value how-many-task"
+                       placeholder={title} />
+            </Label>
+        </Div>
+    );
+}
+
+export default InputField;
---
+++ src/components/InputField/styles.js
@@ -0,0 +1,11 @@
+import styled from "styled-components";
+
+export const Label = styled.label`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+`;
+
+export const Input = styled.input`
+  padding: 5px;
+`;
